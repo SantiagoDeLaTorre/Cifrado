db9726d3b93c098efbd8d45f0f6c6c177549071a
Scottie <Scottie@192.168.1.130>
2014-03-18 13:33:51 +0100

    Archivos de servidor y cliente creados. Primer commit

---
+++ cifrado/src/proyectoeliseo/cliente.java
@@ -0,0 +1,53 @@
+package proyectoeliseo;
+
+import java.io.*;
+import java.net.*;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import org.apache.commons.codec.binary.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+public class cliente{
+
+	public static void main(String[] args) {
+		try
+		{
+			Socket cli = new Socket();
+			InetSocketAddress add = new InetSocketAddress("localhost", 37773);
+			cli.connect(add);
+			InputStream is = cli.getInputStream();;
+			OutputStream os = cli.getOutputStream();
+			
+			//  Recibir Key publica
+			System.out.println("Recibiendo Key Publica");
+			byte[] leo = new byte[256];
+			is.read(leo);
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			X509EncodedKeySpec xeks = new X509EncodedKeySpec(leo);
+			PublicKey we = kf.generatePublic(xeks);
+			System.out.println("Key publica recibida: ");
+			System.out.println(we);
+			
+			// enviar key simetrica encriptada
+			
+			KeyGenerator generoClave = KeyGenerator.getInstance("AES");
+			SecretKey clavePrivadaAES = generoClave.generateKey();
+			String a = Base64.encodeBase64String(clavePrivadaAES.getEncoded());
+			System.out.println("Mi clave privada: " + a);
+			Cipher cp = Cipher.getInstance("RSA");
+			cp.init(Cipher.ENCRYPT_MODE, we);
+			String mCifra = new String(cp.doFinal(a.getBytes()));
+			os.write(mCifra.getBytes());
+			System.out.println("Enviado");
+			
+			
+			//cierre
+			cli.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
---
+++ cifrado/src/proyectoeliseo/servidor.java
@@ -0,0 +1,92 @@
+package proyectoeliseo;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.*;
+import java.net.*;
+
+public class servidor extends Thread{
+
+	private PublicKey  miClavePublica = null;
+	private PrivateKey  miClavePrivada = null;
+	private Socket cliSock = null;
+	private SecretKey clavePriSi = null;
+	private KeyPair miCipher = null;
+	
+	public servidor(Socket so, PublicKey pu, PrivateKey pri, KeyPair ci){
+		this.miClavePrivada = pri;
+		this.miClavePublica = pu;
+		this.cliSock = so;
+		this.miCipher = ci;
+	}
+	
+	public static void main(String[] args) {
+		try{
+			/*ĝenero claves */
+			
+			KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
+			KeyPair kp = keygen.generateKeyPair();
+			ServerSocket miServer = new ServerSocket();
+			InetSocketAddress add = new InetSocketAddress("localhost", 37773);
+			miServer.bind(add);
+			while(miServer != null){
+				Socket nuevoSocket = miServer.accept();
+				System.out.println("Connection Done.");
+				new servidor(nuevoSocket, kp.getPublic(), kp.getPrivate(), kp).start();
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+
+	}
+
+	public void run(){
+		try{
+			System.out.println("run");
+			InputStream is = cliSock.getInputStream();
+			System.out.println("is conectsdo");
+			OutputStream os = cliSock.getOutputStream();
+			System.out.println("os conectado");
+			byte[] array = miClavePublica.getEncoded();
+			Base64 encoder = new Base64();
+			String a = encoder.encodeBase64String(array);
+			os.write(array);
+			System.out.println(new String(a));
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			X509EncodedKeySpec xeks = new X509EncodedKeySpec(array);
+			PublicKey we = kf.generatePublic(xeks);
+			System.out.println(we);
+			
+			//recepcion de clave simetrica 
+			
+			byte[] leo = new byte[256/8];
+			is.read(leo);
+			String aLeo = new String(leo);
+			System.out.println("leo: " + leo);
+			System.out.println("aLeo: " + aLeo);
+			Cipher cp = Cipher.getInstance("RSA");
+			cp.init(Cipher.DECRYPT_MODE, miClavePrivada);
+			System.out.println("I no have idea");
+			String descifro = new String(cp.doFinal(aLeo.getBytes()));
+			System.out.println("Decodeado: " + descifro);
+			byte[] aw = DatatypeConverter.parseHexBinary(descifro);
+			byte[] encodedKey     = Base64.decodeBase64(descifro);
+		    SecretKey oKey = new SecretKeySpec(descifro.getBytes(), 0, encodedKey.length, "AES");
+			String adw = Base64.encodeBase64String(oKey.getEncoded());
+			System.out.println("clave privada " + adw);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+
+}

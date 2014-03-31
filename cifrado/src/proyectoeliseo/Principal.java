5d708411153f6ec06307087e4ddea9de71124f0e
ScottieBullman <scottie.bullman.73@gmail.com>
2014-03-31 20:41:54 +0200

    Principal ahora inicia el servidor. Es un poco inutil la verdad...

--- cifrado/src/proyectoeliseo/Principal.java
+++ cifrado/src/proyectoeliseo/Principal.java
@@ -5,8 +5,6 @@ public class Principal {
     public static void main(String[] args) {
         try{
             new servidorPrincipal().start();
-            Thread.sleep(400);
-            cliente a = new cliente();
         }catch(Exception e){
             e.printStackTrace();
         }
